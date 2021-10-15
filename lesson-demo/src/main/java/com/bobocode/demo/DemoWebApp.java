00a6ed65a3f901fdc7bb929e669922fdbcae6d6d
Andrii Bobrov <Andrii.Bobrov_rft@refinitiv.com>
2021-10-15 13:16:42 +0300

    HM15: -refactor

--- lesson-demo/src/main/java/com/bobocode/demo/DemoWebApp.java
+++ lesson-demo/src/main/java/com/bobocode/demo/DemoWebApp.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.SneakyThrows;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
@@ -15,6 +16,10 @@ import java.net.Socket;
 import java.net.URI;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -22,7 +27,17 @@ public class DemoWebApp {
 
     @SneakyThrows
     public static void main(String[] args) {
-        String photos = getForBody("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=12&api_key=DEMO_KEY");
+        URI photosUri = URI.create("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=12&api_key=DEMO_KEY");
+
+        String photos = doWithinOpenedSocket(photosUri,
+                (writer, reader) -> {
+                    writer.println("GET " + photosUri.getPath() + "?" + photosUri.getQuery() + " HTTP/1.0");
+                    writer.println("Host: " + photosUri.getHost());
+                    writer.println();
+                    writer.flush();
+                    return reader.lines().filter(s -> s.startsWith("{")).findFirst().orElseThrow();
+                });
+
         JsonNode jsonNode = new ObjectMapper().readValue(photos, JsonNode.class);
         List<Image> images = StreamSupport.stream(jsonNode.get("photos").spliterator(), false)
                 .map(node -> node.get("img_src"))
@@ -30,68 +45,54 @@ public class DemoWebApp {
                 .map(Image::new)
                 .collect(Collectors.toList());
 
-        fetchLocation(images);
-        fetchLength(images);
+        doWithinOpenedSocket(URI.create(images.get(0).getInitLocation()), (writer, reader) -> {
+            images.forEach(img ->
+                    img.setLocation(headFor(writer, reader, URI.create(img.getInitLocation()), "Location"))
+            );
+            return null;
+        });
+
+        doWithinOpenedSocket(URI.create(images.get(0).getLocation()), (writer, reader) -> {
+            images.forEach(img -> {
+                        String lengthStr = headFor(writer, reader, URI.create(img.getLocation()), "Content-Length");
+                        img.setSize(Long.parseLong(lengthStr));
+                    }
+            );
+            return null;
+        });
 
         Image maxImage = images.stream()
                 .max(Comparator.comparing(Image::getSize))
                 .orElseThrow();
-        System.out.println(maxImage);
-        post(BobocodeRequest.builder().
-                picture(
-                        Picture.builder()
-                                .url(maxImage.getInitLocation())
-                                .size(maxImage.getSize())
-                                .build()
-                )
-                .user(User.builder().firstName("Andrii").lastName("Bobrov").build())
-                .build());
-    }
 
-    @SneakyThrows
-    private static String getForBody(String url) {
-        URI uri = URI.create(url);
-        var factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-        try (var socket = (SSLSocket) factory.createSocket(uri.getHost(), 443);
-             var writer = new PrintWriter(socket.getOutputStream(), false);
-             var reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+        var request = new BobocodeRequest(
+                        new Picture(maxImage.getInitLocation(), maxImage.getSize()),
+                        new User("Andrii", "Bobrov")
+        );
 
-            writer.println("GET " + uri.getPath() + "?" + uri.getQuery() + " HTTP/1.0");
-            writer.println("Host: " + uri.getHost());
-            writer.println();
+        URI bobocodeServer = URI.create("http://bobocode.herokuapp.com/nasa/pictures");
+        ObjectWriter objectWriter = new ObjectMapper().writerFor(BobocodeRequest.class);
+        String jsonBody = objectWriter.writeValueAsString(request);
+
+        String response = doWithinOpenedSocket(bobocodeServer, (writer, reader) -> {
+            writer.println("POST " + bobocodeServer.getPath() + " HTTP/1.1\r");
+            writer.println("Host: " + bobocodeServer.getHost() + "\r");
+            writer.println("Content-Length: " + jsonBody.length() + "\r");
+            writer.println("Content-Type: application/json;charset=UTF-8\r");
+            writer.println("User-Agent: AnderbSocket/0.1\r");
+            writer.println("\r");
+            writer.println(jsonBody + "\r");
+            writer.println("\r");
+            writer.println("\r");
             writer.flush();
-            return reader.lines().filter(s -> s.startsWith("{")).findFirst().orElseThrow();
-        }
+            System.out.println("Reading response:");
+            return reader.lines().collect(Collectors.joining("\n"));
+        });
+
+        System.out.println(response);
+
     }
 
-    @SneakyThrows
-    private static void fetchLocation(List<Image> images) {
-        try (var socket = new Socket(URI.create(images.get(0).initLocation).getHost(), 80);
-             var writer = new PrintWriter(socket.getOutputStream(), false);
-             var reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-
-            images.forEach(img ->
-                    img.setLocation(headFor(writer, reader, URI.create(img.getInitLocation()), "Location"))
-            );
-        }
-    }
-
-    @SneakyThrows
-    private static void fetchLength(List<Image> images) {
-        var factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-        try (var socket = (SSLSocket) factory.createSocket(URI.create(images.get(0).location).getHost(), 443);
-             var writer = new PrintWriter(socket.getOutputStream(), false);
-             var reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-
-            images.forEach(img -> {
-                        String lengthStr = headFor(writer, reader, URI.create(img.location), "Content-Length");
-                        img.setSize(Long.parseLong(lengthStr));
-                    }
-            );
-        }
-    }
-
-    @SneakyThrows
     private static String headFor(PrintWriter writer, BufferedReader reader, URI uri, String header) {
         writer.println("HEAD " + uri.getPath() + " HTTP/1.1");
         writer.println("Host: " + uri.getHost());
@@ -105,27 +106,20 @@ public class DemoWebApp {
     }
 
     @SneakyThrows
-    private static void post(BobocodeRequest request) {
-        URI uri = URI.create("https://bobocode.herokuapp.com/nasa/pictures");
-        ObjectWriter objectWriter = new ObjectMapper().writerFor(request.getClass());
-        String jsonBody = objectWriter.writeValueAsString(request);
-        try (var socket = new Socket(uri.getHost(), 80);
+    private static String doWithinOpenedSocket(URI uri, BiFunction<PrintWriter, BufferedReader, String> biFunction) {
+        try (var socket = openSocketConnection(uri);
              var writer = new PrintWriter(socket.getOutputStream(), false);
              var reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
-            writer.println("POST " + uri.getPath() + " HTTP/1.1\r");
-            writer.println("Host: " + uri.getHost() + "\r");
-            writer.println("Content-Length: " + jsonBody.length() + "\r");
-            writer.println("Content-Type: application/json;charset=UTF-8\r");
-            writer.println("User-Agent: AnderbSocket/0.1\r");
-            writer.println("\r");
-            writer.println(jsonBody + "\r");
-            writer.println("\r");
-            writer.println("\r");
-            writer.flush();
-            System.out.println("Reading response:");
-            reader.lines().forEach(System.out::println);
+            return biFunction.apply(writer, reader);
         }
+    }
+
+    private static Socket openSocketConnection(URI uri) throws IOException {
+        if (uri.getScheme().equals("https")) {
+            return SSLSocketFactory.getDefault().createSocket(uri.getHost(), 443);
+        }
+        return new Socket(uri.getHost(), 80);
     }
 
     @Data
@@ -143,6 +137,7 @@ public class DemoWebApp {
 
     @Data
     @Builder
+    @AllArgsConstructor
     static class BobocodeRequest {
         private Picture picture;
         private User user;
@@ -150,6 +145,7 @@ public class DemoWebApp {
 
     @Data
     @Builder
+    @AllArgsConstructor
     static class Picture {
         private String url;
         private long size;
@@ -157,6 +153,7 @@ public class DemoWebApp {
 
     @Data
     @Builder
+    @AllArgsConstructor
     static class User {
         private String firstName;
         private String lastName;
